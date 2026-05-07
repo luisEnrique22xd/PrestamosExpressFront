a6f0e443e3a7d13d380b028522c51d8fbe0647b8
Luis Enrique Sanchez Romero <luiskike2210@gmail.com>
2026-05-07 10:23:56 -0600

    Manifest json 2.3

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,16 @@
-'use client';
-import  { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import  Sidebar  from "../components/sidebar"
-import "../globals.css"
+// 1. Quita 'use client' de este archivo (layout.tsx)
+import AuthGuard from "../components/authGuard";
+import Sidebar from "../components/sidebar";
+import "../globals.css";
+
 
 export const metadata = {
   title: 'SAPPE - Préstamos Express',
-  manifest: '/manifest.json', // Esto vincula el archivo que creamos arriba
-  themeColor: '#0047AB',      // Color de la barra de estado en Android
+  manifest: '/manifest.json',
+  themeColor: '#0047AB',
   icons: {
     icon: '/images/icon-192x192.png',
-    apple: '/images/icon-192x192.png', // Específico para iPhone
+    apple: '/images/icon-192x192.png',
   },
 };
 
@@ -19,29 +19,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const router = useRouter();
-  useEffect(() => {
-  const token = localStorage.getItem('access_token');
-  if (!token) {
-    router.push('/login'); // Redirección forzada si no hay token
-  }
-}, []);
-  
   return (
     <html lang="es">
       <body className="bg-gray-100 antialiased text-slate-900">
-        <div className="flex flex-col md:flex-row min-h-screen">
-          
-         
-          <Sidebar />
-
-          <div className="flex-1 flex flex-col min-w-0">
-            <main className="p-4 md:p-10 overflow-y-auto w-full">
-              {children}
-            </main>
+        {/* Usamos un cliente componente solo para la lógica de redirección */}
+        <AuthGuard> 
+          <div className="flex flex-col md:flex-row min-h-screen">
+            <Sidebar />
+            <div className="flex-1 flex flex-col min-w-0">
+              <main className="p-4 md:p-10 overflow-y-auto w-full">
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
+        </AuthGuard>
       </body>
     </html>
-  )
+  );
 }
